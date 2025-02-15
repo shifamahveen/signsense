d56b576fdf13d0dc47c4ac86a33fb67f23d0c162
shifamahveen <mahveenshifa1611@gmail.com>
2025-02-15 18:15:42 +0530

    gesture change

--- controllers/gestureController.js
+++ controllers/gestureController.js
@@ -10,28 +10,26 @@ const gestureController = {
 
     submitImage: async (req, res) => {
         const user = req.session.user;
-
+    
         try {
             if (!req.file) {
                 return res.status(400).send('No file uploaded');
             }
-
-            // Convert Buffer to Blob
+    
             const formData = new FormData();
             formData.append('api_key', 'QlF1LIFoUMhzc4IX6YWXgLXprdfh4kPx');
             formData.append('api_secret', 'raUdHwW8adBldgvAPSGtABUCEwLwmczx');
             formData.append('image_file', req.file.buffer, { filename: req.file.originalname });
-
+    
             const response = await fetch('https://api-us.faceplusplus.com/humanbodypp/v1/gesture', {
                 method: 'POST',
                 body: formData
             });
-
+    
             const data = await response.json();
-
-            // Convert image buffer to base64 for rendering in EJS
+    
             const imageBase64 = `data:${req.file.mimetype};base64,${req.file.buffer.toString('base64')}`;
-
+    
             if (!data.hands || data.hands.length === 0) {
                 return res.render('result', {
                     handGesture: "No hand detected",
@@ -40,19 +38,24 @@ const gestureController = {
                     user
                 });
             }
-
+    
+            const gestures = data.hands[0]?.gesture;
+            const detectedGesture = Object.keys(gestures).reduce((a, b) =>
+                gestures[a] > gestures[b] ? a : b
+            );
+    
             res.render('result', {
-                handGesture: data.hands[0]?.gesture,
+                handGesture: detectedGesture,
                 handRectangle: data.hands[0]?.hand_rectangle,
                 imageUrl: imageBase64,
                 user
             });
-
+    
         } catch (err) {
             console.error('Error:', err);
             res.status(500).send('Error processing the image');
         }
-    }
+    }    
 };
 
 module.exports = gestureController;

e5b2c7bb61958b010f816ba57a14d47b18506d40
shifamahveen <mahveenshifa1611@gmail.com>
2025-02-21 13:28:40 +0530

    added camera

--- routes/gestureRoutes.js
+++ routes/gestureRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 const upload = multer(); // memory storage for image buffer
 
 router.get('/', ensureAuthenticated, gestureController.showIndex);
-router.post('/', ensureAuthenticated, upload.single('image_file'), gestureController.submitImage);
+router.post('/result', ensureAuthenticated, upload.single('image_file'), gestureController.submitImage);
 
 module.exports = router;
